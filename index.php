02fa980c69b59c02f008a1f345a057dd5200e479
RoseLand96 <1716210001@utec-tgo.edu.mx>
2017-03-09 10:20:57 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$db_host = "am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$db_name = "vdljap0ozlv19mcw";
+$db_user = "skcjf9suu6pnya1m";
+$db_password = "y2t9y01b51v2970w";
+$connection = mysqli_connect($db_host, $db_user, $db_password) or die("Connection Error: " . mysqli_error());
+mysqli_select_db($connection,$db_name) or die("Error al seleccionar la base de datos:".mysqli_error());
+@mysqli_query("SET NAMES 'utf8'");
+
+if(isset($_POST["id_contacto"])){
+
+$id_contacto = $_POST["id_contacto"];
+$sql_query = "SELECT * FROM contactos WHERE id_contacto=$id_contacto;";
+$result = mysqli_query($connection,$sql_query);
+$rows = array();
+while($r = mysql_fetch_assoc($result)) {
+$rows[] = $r;
+}
+print json_encode($rows);
+}else
+echo "No existe el contacto";
+mysqli_close($connection);
+?>
